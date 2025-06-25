2ceb27d4c8a559ba8137ab60849afea20412f0f3
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2025-06-25 08:43:00 +0530

    remove api key

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,12 @@
 import React, { useEffect } from "react";
 import Login from "./Login";
 import Browse from "./Browse";
-import Header from "./Header";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { auth } from "../utils/firebase";
-import { useDispatch } from "react-redux";
-import { addUser, removeUser } from "../utils/userSlice";
 
 function Body() {
-  const dispatch = useDispatch();
-
 
   const appRouter = createBrowserRouter([
     {
@@ -26,18 +19,7 @@ function Body() {
     },
   ]);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in
-        const { uid, email, displayName } = user;
-        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
-      } else {
-        // User is signed out
-        dispatch(removeUser());
-      }
-    });
-  }, []);
+
 
   return (
     <div>
