6dc435ac7c867507f1a50069fd515f729be186cd
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2025-07-22 14:20:53 +0530

    add login auth and redux store

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Header from "./Header";
+import { API_OPTIONS } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/movieSlice";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MovieContainer from "./MovieContainer";
+
 
 function Browse() {
+
+  useNowPlayingMovies()
+
   return (
-  <div>
-    <h1>Browse Movie</h1>
+    <>
+  <div className="w-full h-full">
+    <h1><Header/></h1>
   </div>
+  <div>
+    <MovieContainer/>
+  </div>
+    </>
 )}
 
 export default Browse;
---
+++ src/components/MovieContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MovieContainer() {
+  return (
+     <div className="w-36 md:w-48 pr-4">
+      <h1>HELLO</h1>
+    </div>
+  )
+}
+
+export default MovieContainer
