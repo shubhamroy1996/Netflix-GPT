2a5ced177f0fdcf404801aab63add2e9a17964e0
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2025-07-22 22:29:49 +0530

    remove api

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -7,14 +7,6 @@ export const BG_URL =
   export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
-export const API_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: 
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjFjOGQ4ZTUyZGU3ZTMxYmFhNzBhNjZmOTBlYmQ4NSIsIm5iZiI6MTc1MDg0MzQ3My40NzcsInN1YiI6IjY4NWJjMDUxYzA0MmY4ZTJjN2FmYTU5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1eUoQ02YFthYgsqlmwlP4UiWbjkZbix0Y6AmkFe3Gr4"
-  },
-};
 
 export const SUPPORTED_LANGUAGES = [
   { identifier: "en", name: "English" },
